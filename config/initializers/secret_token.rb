ad84b38d73492bfaca7498caf243524c0d8c486c
Lin Zhang <marshluca@gmail.com>
2013-03-20 18:06:10 +0800

    init the integration app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = '725611c8042d349289571f02bd479b17110d7aab70bfb1ef6ba23872f1db8a5cefd05bd7146c446d29e3c7383aa0f72cf92be339addadb97c773a667fd966ad8'
