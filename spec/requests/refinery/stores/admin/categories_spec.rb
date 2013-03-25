7cb7611765600bf4162eb24c3eac57c6eedab0ea
Lin Zhang <marshluca@gmail.com>
2013-03-25 10:18:49 +0800

    add request spec stores.admin.categories

---
+++ spec/requests/refinery/stores/admin/categories_spec.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+require "spec_helper"
+
+describe Refinery do
+  describe "Stores" do
+    describe "Admin" do
+      describe "categories" do
+        login_refinery_user
+
+      end
+    end
+  end
+end
