3b9152b9bae1452cf355295fe134cb039835d937
Lin Zhang <marshluca@gmail.com>
2013-03-26 09:50:19 +0800

    Add Variant model, include related files

---
+++ app/models/refinery/stores/variant.rb
@@ -0,0 +1,9 @@
+module Refinery
+  module Stores
+    class Variant < Refinery::Core::BaseModel
+      belongs_to :product
+
+      attr_accessible :cost_price, :count_on_hand, :deleted_at, :depth, :height, :is_master, :position, :price, :sku, :weight, :width
+    end
+  end
+end
