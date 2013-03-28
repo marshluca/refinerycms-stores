f141efe48e1c485ec178bbdaf432c43689e09843
Lin Zhang <marshluca@gmail.com>
2013-03-28 13:27:44 +0800

    activate Spree::AuthenticationHelpers in engine

--- lib/refinery/stores/engine.rb
+++ lib/refinery/stores/engine.rb
@@ -17,11 +17,20 @@ module Refinery
         end
       end
 
+      def self.activate
+        # Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
+        #   Rails.configuration.cache_classes ? require(c) : load(c)
+        # end
 
+        ::ApplicationController.send :include, Spree::AuthenticationHelpers
+      end
 
       config.after_initialize do
         Refinery.register_extension(Refinery::Stores)
       end
+
+      config.to_prepare &method(:activate).to_proc
+
     end
   end
 end
