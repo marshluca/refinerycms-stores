f72443eb29a45698299f17ee243cf9b9381f359c
Lin Zhang <marshluca@gmail.com>
2013-04-02 10:52:12 +0800

    spec for store menu, product submenu on the refinery layout

--- spec/requests/spree_spec.rb
+++ spec/requests/spree_spec.rb
@@ -12,12 +12,22 @@ describe Refinery do
           page.should have_selector(:title, "Refinery")
           page.should have_content("Listing Orders")
         end
+
+        it "should have store menu on the refinery layout" do
+          visit spree.admin_path
+          page.should have_selector("div#admin_menu")
+        end
       end
 
       describe "GET /store/admin/products" do
         it "should see spree products admin panel" do
           visit spree.admin_products_path
           page.should have_content("Listing Products")
+        end
+
+        it "should have products submenu" do
+          visit spree.admin_products_path
+          page.should have_selector("div#sub-menu")
         end
       end
     end
